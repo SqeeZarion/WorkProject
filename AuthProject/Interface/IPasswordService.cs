049689fc894d47f86ce7ad7b5a1b94272cecdf9e
SqeeZe <yrapetruha0903@gmail.com>
2023-07-29 05:05:02 +0300

    V1Service

---
+++ AuthProject/Interface/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Interface;
+
+public interface IPasswordService
+{
+    public string Encode(string password);
+    public string Decrypt(string password);
+    public bool Verify(string password, string passwordEnc);
+}
---
+++ AuthProject/Interface/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using WebApplication3.Models;
+using WebAuthCommon;
+
+namespace WebApplication1.Interface;
+
+public interface ITokenService
+{
+    public SymmetricSecurityKey GetSymmetricSecurityKey();
+    public string GenerateJWT(UserAccount user);
+}
