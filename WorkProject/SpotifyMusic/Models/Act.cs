451fb55af5adb50af51ece4439d20dea720db5af
SqeeZe <yrapetruha0903@gmail.com>
2024-04-01 01:38:22 +0300

    Revert "Revert "models for Spotify""
    This reverts commit 0280f897b843e93bc0c1d33a37cb9c8182ea705b.

---
+++ WorkProject/SpotifyMusic/Models/Act.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpotifyWebApi.Models;
+
+public class Act
+{
+    [Key]
+    public int ActId { get; set; }
+    public string ActName { get; set; }
+    public string ActDescr { get; set; }
+    public byte[] ActImage { get; set; }
+}
+
+public class ActRecording
+{
+    [Key]
+    public int ActrecordingId { get; set; }
+    
+    [ForeignKey("Recording")]
+    public int RecordingId { get; set; }
+    
+    [ForeignKey("Act")]
+    public int ActId { get; set; }
+}
+
+public class Tblactrecording
+{
+    public int Actrecordingid { get; set; }
+}
+
+public class Recording
+{
+    [Key]
+    public int RecordingId { get; set; }
+    public string RecordingName { get; set; }
+    public double Duration { get; set; }
+    
+    [ForeignKey("Song")]   
+    public int SongId { get; set; }
+    
+    [ForeignKey("Genre")]
+    public int GenreId { get; set; }
+}
+
+public class Genre
+{
+    [Key]
+    public int GenreId { get; set; }
+    public string GenreName { get; set; }
+    public string GenreDescr { get; set; }
+}
---
+++ WorkProject/SpotifyMusic/Models/PlayList.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpotifyWebApi.Models;
+
+public class PlayList
+{
+    [Key]
+    public int PlayListId { get; set; }
+    public string PlayListName { get; set; }
+    public string PlayListDescr { get; set; }
+    public byte[] PlayListPicture { get; set; }
+    public DateTime CreationDate { get; set; }
+    
+    [ForeignKey("PlayListType")]
+    public int PlayListTypeId { get; set; }
+    
+    public virtual ICollection<PlayListRecording> PlayListRecording { get; set; }
+    public virtual ICollection<PlayListType> PlayListType { get; set; }
+    
+}
+
+public class PlayListType
+{
+    [Key]
+    public int PlayListTypeId { get; set; }
+    public string Playlisttypename { get; set; }
+}
+
+public class PlayListRecording
+{
+    [Key]
+    public int PlayListRecordingId { get; set; }
+    
+    [ForeignKey("PlayList")]
+    public int PlayListId { get; set; }
+    
+    [ForeignKey("Recording")]
+    public int RecordingId { get; set; }
+}
