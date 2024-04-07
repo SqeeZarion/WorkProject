2bcd2e231fd599218fc7d168bb5f003e0ee5d211
SqeeZe <yrapetruha0903@gmail.com>
2024-04-07 18:10:30 +0300

    Database

---
+++ WorkProject/SpotifyMusic/Database/DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpotifyWebApi.Models;
+
+namespace SpotifyWebApi.Database;
+
+public class DbConnection : DbContext
+{
+    public DbConnection(DbContextOptions<DbConnection> options) : base(options)
+    {
+    
+    }
+    
+    public DbSet<Tracks> Tracks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tracks>().ToTable("UserLoveTracks");
+        modelBuilder.Entity<Album>().ToTable("UsersLoveAlbums");
+    }
+}
