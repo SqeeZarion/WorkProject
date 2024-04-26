8e51096309af617785c289201d5d705f341d0f34
SqeeZe <yrapetruha0903@gmail.com>
2024-04-26 11:43:24 +0300

    Adding configuration dependencies and Protocol Buffers

---
+++ WorkProject/SpotifyMusic/Auth/Models/TokenResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WorkProject.Auth.Models;
+
+public class TokenResponse
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; }
+
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+}
